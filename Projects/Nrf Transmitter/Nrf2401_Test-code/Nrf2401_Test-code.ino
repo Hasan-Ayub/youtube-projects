cf36777d9bd5f3ebf79e88e74d422e111c32b0df
Hasan Ayub <Hasan-Ayub@users.noreply.github.com>
2026-07-07 22:36:41 +0500

    Add Gerber receiver PCB download to nRF24L01 project page

--- Projects/Nrf Transmitter/Nrf2401_Test-code/Nrf2401_Test-code.ino
+++ Projects/Nrf Transmitter/Nrf2401_Test-code/Nrf2401_Test-code.ino
@@ -1,30 +1,9 @@
-#include <SPI.h>
-#include <nRF24L01.h>
-#include <RF24.h>
-
-RF24 radio(9, 10);   // CE, CSN
-
-void setup() {
-  Serial.begin(115200);
-
-  while (!Serial);
-
-  Serial.println("Checking NRF24L01...");
-
-  if (!radio.begin()) {
-    Serial.println("❌ NRF24L01 NOT detected!");
-    while (1);
-  }
-
-  Serial.println("✅ NRF24L01 detected!");
-
-  radio.setPALevel(RF24_PA_LOW);
-  radio.setDataRate(RF24_1MBPS);
-  radio.setChannel(76);
-
-  Serial.println("\n===== Radio Details =====");
-  radio.printDetails();
+String var = "my name is hassan \n";
+void setup(){
+  Serial.begin(9600);
+  Serial.print(var);
 }
+void loop(){
+Serial.print(var);
 
-void loop() {
 }
